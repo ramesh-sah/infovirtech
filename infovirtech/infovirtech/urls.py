10ca7c42510e9ad1edfde15804c62bf5eb6fa374
Ramesh Kumar Sah <88158556+ramesh-sah@users.noreply.github.com>
2023-09-21 23:15:50 +0545

    Update urls.py

--- infovirtech/infovirtech/urls.py
+++ infovirtech/infovirtech/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from infovirtech import views
 
 
@@ -30,3 +32,6 @@ urlpatterns = [
     path("Login/",views.logiN),
     path("Logout/",views.logouT)
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
